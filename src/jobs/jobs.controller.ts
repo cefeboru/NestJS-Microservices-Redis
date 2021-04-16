c6e4c55d5b2ed67e9ef0a3ac36ed8e995f58d234
Cesar Bonilla <cefeboru@gmail.com>
2021-04-15 21:15:32 -0600

    Get query params correctly for /GET /jobs

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { Job } from './job.model';
 import { JobsService } from './jobs.service';
 
@@ -8,9 +8,9 @@ export class JobsController {
 
   @Get()
   async getProducts(
-    @Param('description') description: string,
-    @Param('city') city: string,
-    @Param('page') page = 0,
+    @Query('description') description: string,
+    @Query('city') city: string,
+    @Query('page') page = 0,
   ): Promise<PaginatedResults<Job>> {
     const jobs = await this.jobsService.getJobs(page);
     return {
