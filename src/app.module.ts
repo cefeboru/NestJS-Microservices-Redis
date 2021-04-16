37cc4b2d51c6dbfcd97ef9f383d2ba5a0952a11b
Cesar Bonilla <cefeboru@gmail.com>
2021-04-15 21:14:56 -0600

    Remove unused controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
 import { JobsModule } from './jobs/jobs.module';
 
 @Module({
   imports: [JobsModule],
   controllers: [],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
